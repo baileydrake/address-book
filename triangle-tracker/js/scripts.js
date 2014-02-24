75957fca9cb459d7fb160693b7b1ab1cdae5c635
Corey Norris & Bailey <student@epicodus.com>
2014-02-24 09:33:48 -0800

    completed address-book project

--- triangle-tracker/js/scripts.js
+++ triangle-tracker/js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $(".contact").click(function() {
+    $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
